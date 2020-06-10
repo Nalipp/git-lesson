13899207dbfff49a9734ffabd40288d63a9eca3f
nate lipp <natelipp@MacBook-Pro.local>
2020-06-09 21:18:02 -0700

    stage 1 of todo app

---
+++ app.js
@@ -0,0 +1,27 @@
+let button = document.querySelector('button');
+let body = document.querySelector('body');
+
+button.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  let input = document.querySelector('input');
+
+  let p = document.createElement('p');
+  p.innerHTML = input.value;
+
+  let span = document.createElement('span');
+  span.innerHTML = ' X';
+
+  span.addEventListener('click', function(e) {
+
+    e.target.parentNode.innerHTML = '';
+  });
+
+  p.append(span);
+  body.append(p);
+
+  input.value = '';
+});
+
+
+
